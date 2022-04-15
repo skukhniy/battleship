a0ef61ab0e6a34278050d45b93222226c9ce07b7
Stan <skukhniy@gmail.com>
2022-04-15 17:46:44 -0400

    init new branch, trying new drag n drop method

--- src/dragndrop.js
+++ src/dragndrop.js
@@ -3,34 +3,3 @@ import createGridBlocks from './render';
 
 createGridBlocks(displayController.boardContainer);
 
-displayController.ship1block.addEventListener('dragstart', () => {
-  console.log('drag start');
-});
-
-function clearDragAbove() {
-  dynamicController()[0].forEach((block) => {
-    block.classList.remove('dragAbove');
-    block.setAttribute('draggable', false);
-  });
-}
-
-dynamicController()[0].forEach((block) => {
-  console.log('block');
-  block.addEventListener('dragover', (e) => {
-    e.preventDefault();
-    clearDragAbove();
-    block.classList.add('dragAbove');
-    block.setAttribute('draggable', true);
-  });
-});
-
-displayController.shipSelectContainer.addEventListener('dragover', (e) => {
-  e.preventDefault();
-  displayController.shipSelectContainer.appendChild(displayController.ship1block);
-  clearDragAbove();
-});
-
-displayController.ship1block.addEventListener('dragend', () => {
-  console.log('drag end');
-  displayController.ship1block.remove();
-});
