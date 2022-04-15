73217cc0375dce0d350af6cd0da17fae66dc0a57
Stan <skukhniy@gmail.com>
2022-04-15 19:04:10 -0400

    added better drop logic

--- src/dragndrop.js
+++ src/dragndrop.js
@@ -3,3 +3,42 @@ import createGridBlocks from './render';
 
 createGridBlocks(displayController.boardContainer);
 
+let dragged;
+
+document.addEventListener('drag', () => {
+
+});
+
+document.addEventListener('dragstart', (e) => {
+  dragged = e.target;
+  console.log(dragged);
+});
+
+document.addEventListener('dragenter', (e) => {
+  console.log('DRAG ENTER');
+  if (e.target.classList.contains('dropzone')) {
+    e.target.style.background = 'red';
+  }
+});
+document.addEventListener('dragover', (e) => {
+  e.preventDefault();
+});
+
+document.addEventListener('dragleave', (e) => {
+  console.log('DRAG LEAVE');
+  if (e.target.classList.contains('dropzone')) {
+    e.target.style.background = 'none';
+  }
+});
+
+document.addEventListener('drop', (e) => {
+  console.log('DROP');
+  e.preventDefault();
+  console.log('test');
+  if (e.target.classList.contains('dropzone')) {
+    console.log(dragged);
+    dragged.parentNode.removeChild(dragged);
+    e.target.appendChild(dragged);
+    e.target.style.background = 'none';
+  }
+});
--- src/render.js
+++ src/render.js
@@ -1,7 +1,7 @@
 function createGridBlocks(container) {
   for (let i = 1; i < 101; i++) {
     const blockDiv = document.createElement('div');
-    blockDiv.className = 'grid_block';
+    blockDiv.classList.add('grid_block', 'dropzone');
     blockDiv.id = `grid${String(i)}`;
     // const openDiv = document.createElement('div');
     // openDiv.className = 'open_block';
