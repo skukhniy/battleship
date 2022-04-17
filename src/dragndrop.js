8cb148b30c3fadd8f2c2a8ab9e086bdae342df25
Stan <skukhniy@gmail.com>
2022-04-17 17:19:08 -0400

    new branch to test different dragging method

--- src/dragndrop.js
+++ src/dragndrop.js
@@ -37,41 +37,6 @@ function isVertical(block) {
   return false;
 }
 
-// if the ship is longer than one block
-// this will return the doc elemets of the other grids touched by the ship during dragging
-function checkPosition(ship, e) {
-  // the array which holds the addition grid elements
-  const elemArray = [];
-  const shipSize = checkSize(ship);
-  // init variables for gridID's and the modifier(moving up or to the left etc.)
-  let modifier = 0;
-  let secondGridID = 0;
-  let thirdGridID = 0;
-  let fourthGridID = 0;
-  if (shipSize === 1) {
-    return undefined;
-  }
-  if (isVertical(ship)) {
-    modifier = 10;
-  } else {
-    modifier = 1;
-  }
-  if (shipSize >= 2) {
-    secondGridID = grabGridID(e) + modifier;
-    const secondGrid = document.getElementById(`grid${String(secondGridID)}`);
-    elemArray.push(secondGrid);
-  } if (shipSize >= 3) {
-    thirdGridID = secondGridID + modifier;
-    const thirdGrid = document.getElementById(`grid${String(thirdGridID)}`);
-    elemArray.push(thirdGrid);
-  } if (shipSize >= 4) {
-    fourthGridID = thirdGridID + modifier;
-    const fourthGrid = document.getElementById(`grid${String(fourthGridID)}`);
-    elemArray.push(fourthGrid);
-  }
-  return (elemArray);
-}
-
 document.addEventListener('drag', () => {
 
 });
@@ -85,18 +50,6 @@ document.addEventListener('dragstart', (e) => {
 // changes the grid to red when a ship is dragged over it
 document.addEventListener('dragenter', (e) => {
   // console.log('DRAG ENTER');
-  if (e.target.classList.contains('dropzone')) {
-    e.target.style.background = 'red';
-    if (checkSize(dragged) > 1) {
-      activeGrids = [e.target.id];
-      console.log(checkPosition(dragged, e.target))
-      checkPosition(dragged, e.target).forEach((elem) => {
-        console.log(elem);
-        elem.style.background = 'red';
-        activeGrids.push(elem.id);
-      });
-    }
-  }
 });
 document.addEventListener('dragover', (e) => {
   e.preventDefault();
@@ -105,19 +58,6 @@ document.addEventListener('dragover', (e) => {
 // removes the background color when the ship leaves this grid area
 document.addEventListener('dragleave', (e) => {
   // console.log('DRAG LEAVE');
-  if (e.target.classList.contains('dropzone')) {
-    if (!activeGrids.includes(e.target.id)) {
-      e.target.style.background = 'none';
-    }
-    if (checkSize(dragged) > 1) {
-      checkPosition(dragged, e.target).forEach((elem) => {
-        console.log(elem.id);
-        if (!activeGrids.includes(elem.id)) {
-          elem.style.background = 'none';
-        }
-      });
-    }
-  }
 });
 
 document.addEventListener('drop', (e) => {
