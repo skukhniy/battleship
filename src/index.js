15af44da7980595a079909e18a396e2a7cd3dbd8
Stan <skukhniy@gmail.com>
2022-05-08 13:37:35 -0400

    merged

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import './style.css';
 import './dragndrop';
 import { renderShipSelection } from './dragndrop_render';
 
-// createBoardBtns();
-// createGridBlocks(displayController.board);
-// createShipSelection([0, 4, 3, 2, 1]);
+createBoardBtns();
+createGridBlocks(displayController.board);
+createShipSelection([0, 4, 3, 2, 1]);
